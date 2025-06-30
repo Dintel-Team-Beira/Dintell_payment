dd9eb4d5c4bb90d6171a32e37ae1a6557dfe9910
Arnaldo tomo <arnaldotomo@gmail.com>
2025-06-30 13:33:11 +0200

    Update company.php

--- config/company.php
+++ config/company.php
@@ -1,16 +1,14 @@
-
 <?php
-// config/company.php
 
 return [
-    'name' => env('COMPANY_NAME', 'SUA EMPRESA, LDA'),
-    'nuit' => env('COMPANY_NUIT', '123456789'),
-    'address' => env('COMPANY_ADDRESS', 'Av. Principal nº 123, R/C'),
+    'name' => env('COMPANY_NAME', 'DINTELL, LDA'),
+    'nuit' => env('COMPANY_NUIT', '401170839'),
+    'address' => env('COMPANY_ADDRESS', 'Av. Maguiguana nº 137 R/C'),
     'city' => env('COMPANY_CITY', 'Maputo'),
     'country' => env('COMPANY_COUNTRY', 'Moçambique'),
-    'phone' => env('COMPANY_PHONE', '+258 84 123 4567'),
-    'email' => env('COMPANY_EMAIL', 'geral@empresa.co.mz'),
-    'website' => env('COMPANY_WEBSITE', 'www.empresa.co.mz'),
+    'phone' => env('COMPANY_PHONE', '866713342'),
+    'email' => env('COMPANY_EMAIL', 'comercial@dintell.co.mz'),
+    'website' => env('COMPANY_WEBSITE', 'www.dintell.co.mz'),
     'logo_url' => env('COMPANY_LOGO_URL', ''),
     'tagline' => env('COMPANY_TAGLINE', 'beyond technology, intelligence.'),
 
@@ -22,7 +20,7 @@ return [
     // Configurações de documentos
     'quote_prefix' => env('QUOTE_PREFIX', 'COT'),
     'invoice_prefix' => env('INVOICE_PREFIX', 'FAT'),
-    'default_tax_rate' => env('DEFAULT_TAX_RATE', 17.00),
+    'default_tax_rate' => env('DEFAULT_TAX_RATE', 16.00), // Atualizado para 16% conforme a fatura
     'default_quote_validity_days' => env('DEFAULT_QUOTE_VALIDITY_DAYS', 30),
     'default_invoice_due_days' => env('DEFAULT_INVOICE_DUE_DAYS', 30),
 ];
