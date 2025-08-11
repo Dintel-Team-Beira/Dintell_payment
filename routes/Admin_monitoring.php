ccbf49659b900eb2b9bb26596bdcd034263a4b3a
Arnaldo tomo <arnaldotomo@gmail.com>
2025-08-11 16:22:43 +0200

    Update Admin_monitoring.php

--- routes/Admin_monitoring.php
+++ routes/Admin_monitoring.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\AdminMonitoringController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::prefix('monitoring')->name('monitoring.')->group(function () {
         Route::get('/performance', [AdminMonitoringController::class, 'performance'])->name('performance');
         Route::get('/health', [AdminMonitoringController::class, 'health'])->name('health');
