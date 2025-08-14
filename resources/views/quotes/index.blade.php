01b62432ac604f0ce3474447ea4e4675b78e5921
Arnaldo tomo <arnaldotomo@gmail.com>
2025-08-14 15:11:43 +0200

    Update index.blade.php

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -6,7 +6,7 @@
 @section('header-actions')
 <div class="flex space-x-3">
     <!-- Botão de Exportar -->
-    {{-- <div class="relative" x-data="{ open: false }">
+    <div class="relative" x-data="{ open: false }">
         <button @click="open = !open" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 transition-colors bg-white border border-gray-300 rounded-lg hover:bg-gray-50">
             <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m-8 9a9 9 0 110-18 9 9 0 010 18z"/>
@@ -43,7 +43,7 @@
                 </a>
             </div>
         </div>
-    </div> --}}
+    </div>
 
     <!-- Botão Nova Cotação -->
     <a href="{{ route('quotes.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white transition-colors bg-green-600 rounded-lg hover:bg-green-700">
@@ -417,7 +417,7 @@
                                 @elseif($quote->valid_until->isTomorrow())
                                     Expira amanhã
                                 @elseif($quote->valid_until->diffInDays() <= 7)
-                                    Expira em {{ $quote->valid_until->diffInDays() }} dias
+                                    Expira em {{abs(number_format( $quote->valid_until->diffInDays())) }} dias
                                 @else
                                     {{ $quote->valid_until->diffForHumans() }}
                                 @endif
